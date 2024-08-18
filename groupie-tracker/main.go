5f1cdd78ad444d6693122db73d78f03b0dfeccf7
heyZakaria <zaki.abdo82@gmail.com>
2024-08-18 16:51:17 +0100

    first commmit

---
+++ groupie-tracker/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"functions/functions"
+)
+
+func main() {
+	http.HandleFunc("/static/", functions.SetupStaticFilesHandlers)
+	http.HandleFunc("/", functions.Handler)
+	log.Println("Server starting on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
